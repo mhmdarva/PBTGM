4b76411c8ae83fd82f73e159478f25e0fe883989
mhmdarva <mhmdarva3@gmail.com>
2024-08-28 13:52:40 +0700

    Add files via upload

---
+++ Bab2soal2Java.java/src/bab2soal2java/java/Bab2soal2JavaJava.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal2java.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab2soal2JavaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static String Makanan;
+    
+    public static final String Minuman = "Jus jambu";
+    
+    public static void main(String[] args) {
+        Makanan = "Ayam Geprek";
+        
+       
+        System.out.println("Makanan Favorit saya: "  +   Makanan);
+        System.out.println("Minuman Favorit saya: "  +   Minuman);
+    }
+    
+}
