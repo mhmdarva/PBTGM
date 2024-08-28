a1fae98e59c8313f996060258290abceabfc40ec
mhmdarva <mhmdarva3@gmail.com>
2024-08-28 13:59:17 +0700

    Add files via upload

---
+++ Bab6soala1Java/src/main/java/com/mycompany/bab6soala1java/Bab6soala1Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab6soala1java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab6soala1Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
