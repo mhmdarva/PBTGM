eb344a607cb72ad8c9d48e929953ce4f1d611ec1
mhmdarva <mhmdarva3@gmail.com>
2024-08-28 13:54:05 +0700

    Add files via upload

---
+++ Bab4soal1Java.java/src/bab4soal1java/java/Bab4soal1JavaJava.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab4soal1java.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab4soal1JavaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static void main(String[] args) {
+         //inisialisasi verbal
+        int Angka1 = 10;
+        int Angka2 = 3;
+        
+        //operasi aritmatika 
+        int penambahan = Angka1 + Angka2;
+        int pengurangan = Angka1 - Angka2;
+        int perkalian = Angka1 * Angka2;
+        double pembagian = (double) Angka1 / Angka2;
+        
+        //memanggil hasil operasi
+        System.out.println("hasil pertambahan A+B = " + penambahan);
+        System.out.println("hasil pengurangan A-B = " + pengurangan);
+        System.out.println("hasil perkalian A*B = " + perkalian);
+        System.out.println("hasil pembagian A/B = " + pembagian);
+    }
+    
+}
