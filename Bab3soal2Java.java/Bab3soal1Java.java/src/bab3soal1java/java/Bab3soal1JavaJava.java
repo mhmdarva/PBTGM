52597a281d937d547761c36a7a3c65f4db3d2efe
mhmdarva <mhmdarva3@gmail.com>
2024-08-28 13:53:33 +0700

    Add files via upload

---
+++ Bab3soal2Java.java/Bab3soal1Java.java/src/bab3soal1java/java/Bab3soal1JavaJava.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal1java.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab3soal1JavaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+   public static void main(String[] args) {
+        byte a = 9;
+        float f1 = 8.99f;
+        System.out.println (8.99f);
+        char A = 'A';
+        boolean b = false;
+        
+    }
+    
+}
---
+++ Bab3soal2Java.java/src/bab3soal2java/java/Bab3soal2JavaJava.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab3soal2java.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab3soal2JavaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static String mahasiswa = "Andika Jaya Putra";
+    public static String kelas = "01SIFE020";
+   public static void main(String[] args) { 
+       // TODO code applicationlogic here
+int a=2110117000;
+float f = 19.5f;
+double d = 1.72d;
+System.out.println("Nim              : " + a);
+System.out.println  ("Nama Mahasiswa : " + mahasiswa);
+System.out.println("Kelas            : " + kelas);
+System.out.println("Usia             : " + f);
+System.out.println("Tinggi           : " + d);
+    }
+    
+}
