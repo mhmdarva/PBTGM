1e1683990950cb53c75f63f09c188d4867765af7
mhmdarva <mhmdarva3@gmail.com>
2024-08-28 13:51:56 +0700

    Add files via upload

---
+++ Bab2soal1.java/src/bab2soal1/java/Bab2soal1Java.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal1.java;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab2soal1Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Overflow
+        int a=50;
+        float b=a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
